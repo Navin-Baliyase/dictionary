131866c678ca49203c99aa6570f2ee6f330a11a5
Navin Kumar <navinkumar2508@gmail.com>
2019-02-17 15:20:32 +0530

    TECH:Navin | mapping numbers to words | logic

--- app/controllers/icicles_controller.rb
+++ app/controllers/icicles_controller.rb
@@ -1,4 +1,5 @@
 class IciclesController < ApplicationController
+  #require 'number_to_word'
   before_action :set_icicle, only: [:show, :edit, :update, :destroy]
 
   # GET /icicles
@@ -11,6 +12,36 @@ class IciclesController < ApplicationController
   # GET /icicles/1.json
   def show
     @data = File.read(@icicle.upload_path)
+    @path = @icicle.upload_path
+    digits = @icicle.phone.to_s
+    letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
+    dictionary = []
+    file_path = @path
+    File.foreach( file_path ) do |word|
+      dictionary.push word.chop.to_s.downcase
+    end
+    keys = digits.chars.map{|digit|letters[digit]}
+
+    results = {}
+    total_number = keys.length - 1
+    for i in (2..total_number)
+      first_array = keys[0..i]
+      second_array = keys[i + 1..total_number]
+      next if first_array.length < 3 || second_array.length < 3
+      first_combination = first_array.shift.product(*first_array).map(&:join)
+      next if first_combination.nil?
+      second_combination = second_array.shift.product(*second_array).map(&:join)
+      next if second_combination.nil?
+      results[i] = [(first_combination & dictionary), (second_combination & dictionary)]
+    end
+    @final_words = []
+    results.each do |key, combinataions|
+      next if combinataions.first.nil? || combinataions.last.nil?
+      combinataions.first.product(combinataions.last).each do |combo_words|
+        @final_words << combo_words
+      end
+    end
+    @final_words << (keys.shift.product(*keys).map(&:join) & dictionary).join(", ")
   end
 
   # GET /icicles/new
@@ -72,4 +103,4 @@ class IciclesController < ApplicationController
     def icicle_params
       params.require(:icicle).permit(:phone, :upload)
     end
-end
+  end
--- app/models/icicle.rb
+++ app/models/icicle.rb
@@ -1,12 +1,14 @@
 class Icicle < ApplicationRecord
+	#include NumberToWord
 	has_one_attached :upload
+	validates :phone, presence: true
 	validate :file_presence
 
 	def upload_path
-    ActiveStorage::Blob.service.path_for(upload.key)
-    end
+		ActiveStorage::Blob.service.path_for(upload.key)
+	end
 
-    private
+	private
 	def file_presence
 		if upload.attached? == false
 			errors.add(:upload, "is missing.")
